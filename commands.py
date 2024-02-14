c376b9a4f426a8656defaaf3b249b27d51c69264
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-14 09:26:53 +0900

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,3 +1,17 @@
+##Ubuntu 上安裝 git 並抓取程式碼
+##安裝指令
+##sudo apt-get install git
+##建立目錄
+##mkdir repo
+##cd repo
+##初始化目錄（產生 .git 檔案)
+##sudo git init
+##連線
+##git clone https://github.com/Victakashi/20240213_Ubuntu.git
+
+
+
+
 def get_commands():
     commands = [
         ('lscpu | grep "Architecture\|Model name\|CPU(s)"', "CPU information"),  # Extract important information from lscpu command
