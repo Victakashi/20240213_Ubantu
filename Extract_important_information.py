99ce9330ea849ab0a5245922beeb0e7dce719435
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-14 10:54:40 +0900

    Update Extract_important_information.py

--- Extract_important_information.py
+++ Extract_important_information.py
@@ -17,7 +17,6 @@ def main():
         print(output.strip())  # Print the output of the command
         print("")
         print("=" * 40)
-        print("")
 if __name__ == "__main__":
     main()
 
