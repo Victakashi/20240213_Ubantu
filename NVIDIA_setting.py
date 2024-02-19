c8b28323c9afabafded8f96c1a3764cab6936236
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-19 17:50:23 +0900

    Update NVIDIA_setting.py

--- NVIDIA_setting.py
+++ NVIDIA_setting.py
@@ -1,6 +1,13 @@
 
 
 
+#Ubuntu 22.04 LTS如何安裝Nvidia顯示卡驅動、CUDA、cuDNN、NVIDIA Container Toolkit套件
+#https://ivonblog.com/posts/ubuntu-install-nvidia-drivers/
+#CUDA Toolkit Archive
+#https://developer.nvidia.com/cuda-toolkit-archive
+
+
+
 
 
 
