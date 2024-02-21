810767031125a7a0e81719a9db893c3b43a485de
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-21 10:31:12 +0900

    Update NVIDIA_setting.py

--- NVIDIA_setting.py
+++ NVIDIA_setting.py
@@ -5,6 +5,9 @@
 #https://ivonblog.com/posts/ubuntu-install-nvidia-drivers/
 #CUDA Toolkit Archive
 #https://developer.nvidia.com/cuda-toolkit-archive
+#How to Install JetPack #MIC-713(S)-OX4A1 (16GB) MIC-AI Jetpack BSP Support to JetPack 5.1.2
+#https://docs.nvidia.com/jetson/archives/jetpack-archived/jetpack-512/install-jetpack/index.html
+
 
 
 
