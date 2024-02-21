285d03a0dc162908511344c4829e1acbb5c9218f
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-21 14:23:44 +0900

    Update NVIDIA_setting.py

--- NVIDIA_setting.py
+++ NVIDIA_setting.py
@@ -1,12 +1,27 @@
 
+##VNC Ubuntu #buntucto
+#https://youtu.be/_av-tO3dHAI?feature=shared
+#https://www.gingerdoc.com/tutorials/how-to-install-and-configure-vnc-on-ubuntu-20-04
 
 
 #Ubuntu 22.04 LTS如何安裝Nvidia顯示卡驅動、CUDA、cuDNN、NVIDIA Container Toolkit套件
 #https://ivonblog.com/posts/ubuntu-install-nvidia-drivers/
 #CUDA Toolkit Archive
 #https://developer.nvidia.com/cuda-toolkit-archive
-#How to Install JetPack #MIC-713(S)-OX4A1 (16GB) MIC-AI Jetpack BSP Support to JetPack 5.1.2
+
+#MIC-713(S)-OX4A1 (16GB) MIC-AI Jetpack BSP Support to JetPack 5.1.2
+#JetPack 5.1.2
+#https://developer.nvidia.com/embedded/jetpack-sdk-512
+#How to Install JetPack 
 #https://docs.nvidia.com/jetson/archives/jetpack-archived/jetpack-512/install-jetpack/index.html
+#1.3.1. Install JetPack Components on Jetson Linux
+#apt depends nvidia-jetpack | awk '{print $2}' | xargs -I {} sudo apt install -y {}
+#
+
+
+
+
+
 
 
 
