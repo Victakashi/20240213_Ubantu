a01bd4a226eac2984bbb9b318752a3db6ae5c535
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-22 09:51:52 +0900

    Update NVIDIA_setting.py

--- NVIDIA_setting.py
+++ NVIDIA_setting.py
@@ -2,7 +2,7 @@
 ##VNC Ubuntu #buntucto
 #https://youtu.be/_av-tO3dHAI?feature=shared
 #https://www.gingerdoc.com/tutorials/how-to-install-and-configure-vnc-on-ubuntu-20-04
-
+#>vncserver 建立連線,>vncserver -kill 刪除連線
 
 #Ubuntu 22.04 LTS如何安裝Nvidia顯示卡驅動、CUDA、cuDNN、NVIDIA Container Toolkit套件
 #https://ivonblog.com/posts/ubuntu-install-nvidia-drivers/
