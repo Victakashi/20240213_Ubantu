02469eb6c33d290d8337b22ec96a2fc9a9fa030c
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-02-26 10:48:40 +0900

    Update commands.py

--- commands.py
+++ commands.py
@@ -17,8 +17,8 @@ def get_commands():
         ('lsusb | grep "Bus\|Device\|ID"', "USB information"),  # Extract important information from lsusb command
         #('uname -a | grep "Linux"', "Linux version information"),  # Extract important information from uname -a command
         ('lsb_release -a | grep -E "Description ID|Distributor|Release"', "Linux version information"),  # Extract important information from uname -a command
-        #('sudo dmidecode -t bios | grep "Vendor\|Version\|Release Date"', "BIOS information")  # Extract important information from dmidecode command
         ('nvcc --version', "NVIDIA CUDA Version")
+        ('sudo dmidecode -t bios | grep "Vendor\|Version\|Release Date"', "BIOS information")  # Extract important information from dmidecode command
     
     ]
     return commands
