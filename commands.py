cbd13bf30ce9296a5f00525f27266f29d110b489
Victakashi <95895366+Victakashi@users.noreply.github.com>
2024-03-05 09:36:36 +0900

    Update commands.py

--- commands.py
+++ commands.py
@@ -6,8 +6,8 @@ def get_commands():
         ('lsusb | grep "Bus\|Device\|ID"', "USB information"),  # Extract important information from lsusb command
         #('uname -a | grep "Linux"', "Linux version information"),  # Extract important information from uname -a command
         ('lsb_release -a | grep -E "Description ID|Distributor|Release"', "Linux version information"),  # Extract important information from uname -a command
-        ('nvcc --version', "NVIDIA CUDA Version information")
-        ('sudo dmidecode -t bios | grep "Vendor\|Version\|Release Date"', "BIOS information")  # Extract important information from dmidecode command
+        #('nvcc --version', "NVIDIA CUDA Version information")
+        #('sudo dmidecode -t bios | grep "Vendor\|Version\|Release Date"', "BIOS information")  # Extract important information from dmidecode command
          ##要解一下 輸出問題 NVIDIA CUDA BIOS information 有衝突??
     ]
     return commands
